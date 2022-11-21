5f67238e4a80ad60aba4c7fc08a8684e8c92b8f4
Sayanaann <alexsayana@gmail.com>
2022-11-21 14:20:34 +0530

    larg

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int largest(int a, int b,int c){
-         int largest= c > (a > b ? a : b) ? c : ((a > b) ? a : b);
-         return largest;
+    public static int largest(int a, int b, int c) {
+        int largest = c > (a > b ? a : b) ? c : ((a > b) ? a : b);
+        return largest;
     }
 
 
 
     public static void main(String[] args) {
         System.out.println("hi");
-}
+}}
